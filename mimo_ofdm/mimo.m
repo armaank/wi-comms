06b57675fd2aae46078e0d1be5c2f712d83251b8
armaank <kohli@cooper.edu>
2020-04-22 22:51:44 -0400

    finished zf mimo

--- mimo_ofdm/mimo.m
+++ mimo_ofdm/mimo.m
@@ -21,24 +21,25 @@ snr = EbNo + 10*log10(K);
 ber = zeros(3, n_iter, length(snr));
 
 % gaussian channel
-H1 = sqrt(1/2)*(randn(Nrx,Ntx,n_syms*K,n_iter) +...
-    1j*randn(Nrx,Ntx,n_syms*K,n_iter));
+H1 = sqrt(1/2)*(randn(Nrx, Ntx, n_syms*K, n_iter) +...
+    1j*randn(Nrx, Ntx, n_syms*K, n_iter));
 
-% precoding mimo
-U = zeros(Nrx,Ntx,n_syms*K);
-V = zeros(Nrx,Ntx,n_syms*K);
-S = zeros(Nrx,Nrx,n_syms*K);
+%% precoding mimo
 
-precode = zeros(Nrx,1,n_syms*K);
-postcode = zeros(Nrx,1,n_syms*K);
+U = zeros(Nrx, Ntx, n_syms*K);
+S = zeros(Nrx, Ntx, n_syms*K);
+V = zeros(Nrx, Nrx, n_syms*K);
+
+precode = zeros(Nrx, 1, n_syms*K);
+postcode = zeros(Nrx, 1, n_syms*K);
 
 for ii = 1:n_iter
 
-    %generate and modulate random message
+    % generate and modulate random message
     bits = randi([0 M-1], Ntx, 1, n_syms*K);     
-    tx = qammod(bits,M); 
+    tx = qammod(bits, M); 
     
-    %pre-coding
+    % apply channel and perform pre-coding
     for kk=1:n_syms*K
         [U(:,:,kk),S(:,:,kk),V(:,:,kk)] = svd(H1(:,:,kk,ii));
         precode(:,:,kk) = H1(:,:,kk,ii)*V(:,:,kk)*tx(:,:,kk);
@@ -46,18 +47,19 @@ for ii = 1:n_iter
 
     for jj = 1:length(snr)
         
-        n = sqrt(1/2)*(randn(Nrx,1,n_syms*K)...
-            +1j*randn(Nrx,1,n_syms*K));
+        n = sqrt(1/2)*(randn(Nrx, 1, n_syms*K)...
+            +1j*randn(Nrx, 1, n_syms*K));
         
         tx = precode + 10^(-1*snr(jj)/20)*n;
         
-        %post coding
+        % post-coding
         for kk=1:n_syms*K
             postcode(:,:,kk) = (S(:,:,kk)^-1)*U(:,:,kk)'*tx(:,:,kk);
         end
+        
         rx = qamdemod(postcode,M);
 
-        %Compute and store the BER for this iteration
+        % compute ber curve
         [~, ber(1,ii,jj)] = biterr(bits, rx);
 
    end
@@ -65,7 +67,43 @@ end
 
 %% zf mimo
 
+zf_eq = zeros(Nrx, 1, n_syms*K);
+txchan = zeros(Nrx, 1, n_syms*K); 
+W = zeros(Nrx, Ntx, n_syms*K);
+
+for ii = 1:n_iter
+
+    % generate and modulate random message
+    bits = randi([0 M-1], Ntx, 1, n_syms*K);     
+    tx = qammod(bits, M); 
+    
+    % apply channel 
+    for kk=1:n_syms*K
+        txchan(:,:,kk) = H1(:,:,kk,ii)*tx(:,:,kk);
+    end
+
+    for jj = 1:length(snr)
+        
+        n = sqrt(1/2)*(randn(Nrx, 1, n_syms*K)...
+            +1j*randn(Nrx, 1, n_syms*K));
+        
+        tx = txchan + 10^(-1*snr(jj)/20)*n;
+        
+        % zf equalizer
+        for kk=1:n_syms*K
+            W(:,:,kk) = (H1(:,:,kk,ii)'*H1(:,:,kk,ii))^-1*H1(:,:,kk,ii)';
+            zf_eq(:,:,kk) = W(:,:,kk)*tx(:,:,kk);
+        end
+        rx = qamdemod(zf_eq,M);
+
+        % compute ber curve
+        [~, ber(2,ii,jj)] = biterr(bits, rx);
+
+   end
+end
+
 %% mmse mimo
+
 
 %% ploting
 
