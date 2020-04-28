fe2a1a1cb7f1fbe4eb46f72ac637b147b2637a4c
armaank <kohli@cooper.edu>
2020-04-27 22:04:30 -0400

    finished ofdm, starting mimo_ofdm

--- mimo_ofdm/mimo.m
+++ mimo_ofdm/mimo.m
@@ -7,7 +7,7 @@ M = 16;
 K = log2(M);     
 
 % symbols per packet 
-n_syms = 1e4;
+N_syms = 1e4;
 
 % number of channels
 n_chan = 3;
@@ -21,39 +21,39 @@ snr = EbNo + 10*log10(K);
 ber = zeros(3, n_chan, length(snr));
 
 % three different flat fading channels 
-H = sqrt(1/2)*(randn(Nrx, Ntx, n_syms*K, n_chan) +...
-    1j*randn(Nrx, Ntx, n_syms*K, n_chan));
+H = sqrt(1/2)*(randn(Nrx, Ntx, N_syms*K, n_chan) +...
+    1j*randn(Nrx, Ntx, N_syms*K, n_chan));
 
 %% precoding mimo
 
-U = zeros(Nrx, Ntx, n_syms*K);
-S = zeros(Nrx, Ntx, n_syms*K);
-V = zeros(Nrx, Nrx, n_syms*K);
+U = zeros(Nrx, Ntx, N_syms*K);
+S = zeros(Nrx, Ntx, N_syms*K);
+V = zeros(Nrx, Nrx, N_syms*K);
 
-precode = zeros(Nrx, 1, n_syms*K);
-postcode = zeros(Nrx, 1, n_syms*K);
+precode = zeros(Nrx, 1, N_syms*K);
+postcode = zeros(Nrx, 1, N_syms*K);
 
 for ii = 1:n_chan
 
     % generate and modulate random message
-    bits = randi([0 M-1], Ntx, 1, n_syms*K);     
+    bits = randi([0 M-1], Ntx, 1, N_syms*K);     
     tx = qammod(bits, M); 
     
     % apply channel and perform pre-coding
-    for kk=1:n_syms*K
+    for kk=1:N_syms*K
         [U(:,:,kk),S(:,:,kk),V(:,:,kk)] = svd(H(:,:,kk,ii));
         precode(:,:,kk) = H(:,:,kk,ii)*V(:,:,kk)*tx(:,:,kk);
     end
 
     for jj = 1:length(snr)
         
-        n = sqrt(1/2)*(randn(Nrx, 1, n_syms*K)...
-            +1j*randn(Nrx, 1, n_syms*K));
+        n = sqrt(1/2)*(randn(Nrx, 1, N_syms*K)...
+            +1j*randn(Nrx, 1, N_syms*K));
         
         tx = precode + 10^(-1*snr(jj)/20)*n;
         
         % post-coding
-        for kk=1:n_syms*K
+        for kk=1:N_syms*K
             postcode(:,:,kk) = (S(:,:,kk)^-1)*U(:,:,kk)'*tx(:,:,kk);
         end
         
@@ -67,30 +67,30 @@ end
 
 %% zf mimo
 
-zf_eq = zeros(Nrx, 1, n_syms*K);
-txchan = zeros(Nrx, 1, n_syms*K); 
-W = zeros(Nrx, Ntx, n_syms*K);
+zf_eq = zeros(Nrx, 1, N_syms*K);
+txchan = zeros(Nrx, 1, N_syms*K); 
+W = zeros(Nrx, Ntx, N_syms*K);
 
 for ii = 1:n_chan
 
     % generate and modulate random message
-    bits = randi([0 M-1], Ntx, 1, n_syms*K);     
+    bits = randi([0 M-1], Ntx, 1, N_syms*K);     
     tx = qammod(bits, M); 
     
     % apply channel 
-    for kk=1:n_syms*K
+    for kk=1:N_syms*K
         txchan(:,:,kk) = H(:,:,kk,ii)*tx(:,:,kk);
     end
 
     for jj = 1:length(snr)
         
-        n = sqrt(1/2)*(randn(Nrx, 1, n_syms*K)...
-            +1j*randn(Nrx, 1, n_syms*K));
+        n = sqrt(1/2)*(randn(Nrx, 1, N_syms*K)...
+            +1j*randn(Nrx, 1, N_syms*K));
         
         tx = txchan + 10^(-1*snr(jj)/20)*n;
         
         % zf equalizer
-        for kk=1:n_syms*K
+        for kk=1:N_syms*K
             W(:,:,kk) = (H(:,:,kk,ii)'*H(:,:,kk,ii))^-1*H(:,:,kk,ii)';
             zf_eq(:,:,kk) = W(:,:,kk)*tx(:,:,kk);
         end
@@ -106,30 +106,30 @@ end
 
 %% mmse mimo
 
-mmse_eq = zeros(Nrx, 1, n_syms*K);
-txchan = zeros(Nrx, 1, n_syms*K); 
-W = zeros(Nrx, Ntx, n_syms*K);
+mmse_eq = zeros(Nrx, 1, N_syms*K);
+txchan = zeros(Nrx, 1, N_syms*K); 
+W = zeros(Nrx, Ntx, N_syms*K);
 
 for ii = 1:n_chan
 
     % generate and modulate random message
-    bits = randi([0 M-1], Ntx, 1, n_syms*K);     
+    bits = randi([0 M-1], Ntx, 1, N_syms*K);     
     tx = qammod(bits, M); 
     
     % apply channel 
-    for kk=1:n_syms*K
+    for kk=1:N_syms*K
         txchan(:,:,kk) = H(:,:,kk,ii)*tx(:,:,kk);
     end
 
     for jj = 1:length(snr)
         
-        n = sqrt(1/2)*(randn(Nrx, 1, n_syms*K)...
-            +1j*randn(Nrx, 1, n_syms*K));
+        n = sqrt(1/2)*(randn(Nrx, 1, N_syms*K)...
+            +1j*randn(Nrx, 1, N_syms*K));
         
         tx = txchan + 10^(-1*snr(jj)/20)*n;
         
         % mmse equalizer
-        for kk=1:n_syms*K
+        for kk=1:N_syms*K
             
             W(:,:,kk) = (H(:,:,kk,ii)'*H(:,:,kk,ii) + ...
                 eye(Ntx)*10^(-1*snr(jj)/20))^-1 * H(:,:,kk,ii)';
--- mimo_ofdm/ofdm.m
+++ mimo_ofdm/ofdm.m
@@ -2,48 +2,166 @@
 clc; clear all;
 %% simulation parameters
 
-nIter = 3;  % The number of iterations of the simulation
+% modulation order
+M = 16;
+K = log2(M);     
 
-N = 64; % number of subcarriers
+% symbols per transmission 
+N_syms = 1e4;
+
+% number of channels
+n_chan = 3;
+
+EbNo = -10:2:30;                  
+snr = EbNo + 10*log10(K);
+ber = zeros(3, n_chan, length(snr));
+
+%params for frequency selective channel models
+Ts = 1e-3;
+Fd1 = 0;
+Fd2 = 10;
+tau = [0 Ts 2*Ts]; 
+pdb = [0 5 10]; 
+H = [rayleighchan(Ts, Fd1, tau, pdb), ...
+     rayleighchan(Ts, Fd2, tau, pdb),...
+     ricianchan(Ts, Fd1, tau, pdb)];   
+
+% number of fft/ifft points
+npt = 64; 
+
+% 802.11a frame params
+N = 64; % number of subcarriers per ofdm burst
+N_d_syms = 48; % number of data symbols per ofdm burst
+N_gi = 12; % number of gaurd symbols per ofdm burst
 mu = 16; % length of cyclic prefix
-nSamp = N + mu; % total number of samples per ofdm symbol period
-M = 16; % modulation order for QAM
-npt = 64; % number of fft/ifft points
+N_samp = N + mu; % total number of symbols per ofdm burst
 
+% frame indicies 
+pilot_idx = [11 25 39 53]; 
+% todo: fix this
+data_idx = [1:5 7:19 21:26 28:33 35:47 49:53]+5; %frame parameters
 
-chan1 = [.1 .2 .4]; % Somewhat invertible channel, moderate ISI
-chan2 = [0.227 0.460 0.688 0.460 0.227]; % Not so invertible, severe ISI
+%% zf ofdm
 
-EbNo = -5:2:20; % 
-snr_vec = EbNo + 10*log10(N/nSamp); % snr conversion from EbNo
-lenSNR = length(snr_vec); 
+for ii = 1:n_chan
 
-ber = zeros(2,lenSNR,nIter);
-
-%% wls - ofdm zf 
-
-for ii = 1:nIter
+    % generate and modulate random message
+    bits = randi([0,M-1],1, N_syms*N_d_syms);
+    mod_data = qammod(bits,M);  
     
-%     bits = randi([0,M-1],1, nSym*(N-mu));     % Generate random bits
-    mod_data = qammod(bits,M);  % modulate the signal
+    % set up ofdm frame
+    ofdm_data = reshape(mod_data,N_d_syms,[]);
+    ofdm_frame = zeros(N,N_syms);
+    ofdm_frame(data_idx,:) = ofdm_data;
+    ofdm_frame(pilot_idx,:) = 1;
     
+    % add cyclic prefix 
+    ifft_ofdm = ifft(ofdm_frame,npt);
+    ofdm_tx = [ifft_ofdm(N_d_syms+1:N,:); ifft_ofdm];
+      
+    % select channel model
+    Hchan = H(:,ii);
+    chan = zeros(80,N_syms);
+    ofdm_tx_chan = zeros(80,N_syms);
+    for k=1:N_syms
+        chan(:,k) = filter(Hchan,ones(80,1));
+        ofdm_tx_chan(:,k) = chan(:,k).*ofdm_tx(:,k); %apply channel to signal
+    end
+
+    for jj = 1:length(snr)
+        
+        % add awgn noise
+        n = sqrt(1/2)*(randn(N_samp, N_syms)...
+            +1j*randn(N_samp, N_syms));
+        
+        rxchan = ofdm_tx_chan + 10^(-1*snr(jj)/20)*n;
+        
+        % deconstruct ofdm frame
+        rx_rmgi = rxchan(mu+1:end,:);
+        rx_ofdm_frame = fft(rx_rmgi, npt);
+        
+        % zf equilizer 
+        rx_ofdm_frame_zf = rx_ofdm_frame ./ chan(mu+1:end,:);
+        
+        % un-pack data, demodulate
+        rx_ofdm_data = rx_ofdm_frame_zf(data_idx,:);
+        rx_ofdm_mod = reshape(rx_ofdm_data, 1, []);
+        rx = qamdemod(rx_ofdm_mod, M);
+
+        % compute ber curve
+        [~, ber(1,ii,jj)] = biterr(bits, rx);
+
+   end
 end
+
+%% mmse ofdm
+
+for ii = 1:n_chan
+
+    % generate and modulate random message
+    bits = randi([0,M-1],1, N_syms*N_d_syms);
+    mod_data = qammod(bits,M);  
+    
+    % set up ofdm frame
+    ofdm_data = reshape(mod_data,N_d_syms,[]);
+    ofdm_frame = zeros(N,N_syms);
+    ofdm_frame(data_idx,:) = ofdm_data;
+    ofdm_frame(pilot_idx,:) = 1;
+    
+    % add cyclic prefix 
+    ifft_ofdm = ifft(ofdm_frame,npt);
+    ofdm_tx = [ifft_ofdm(N_d_syms+1:N,:); ifft_ofdm];
+      
+    % select channel model
+    Hchan = H(:,ii);
+    chan = zeros(80,N_syms);
+    ofdm_tx_chan = zeros(80,N_syms);
+    for k=1:N_syms
+        chan(:,k) = filter(Hchan,ones(80,1));
+        ofdm_tx_chan(:,k) = chan(:,k).*ofdm_tx(:,k); %apply channel to signal
+    end
+
+    for jj = 1:length(snr)
+        
+        % add awgn noise
+        n = sqrt(1/2)*(randn(N_samp, N_syms)...
+            +1j*randn(N_samp, N_syms));
+        
+        rxchan = ofdm_tx_chan + 10^(-1*snr(jj)/20)*n;
+        
+        % deconstruct ofdm frame
+        rx_rmgi = rxchan(mu+1:end,:);
+        rx_ofdm_frame = fft(rx_rmgi, npt);
+        
+        % mmse equilizer 
+        norm = conj(chan(mu+1:end,:)).*chan(mu+1:end,:) + ...
+            10^(-1*snr(jj)/20);
+        
+        rx_ofdm_frame_zf = rx_ofdm_frame.*conj(chan(mu+1:end,:)) ./ norm;
+        
+        % un-pack data, demodulate
+        rx_ofdm_data = rx_ofdm_frame_zf(data_idx,:);
+        rx_ofdm_mod = reshape(rx_ofdm_data, 1, []);
+        rx = qamdemod(rx_ofdm_mod, M);
+
+        % compute ber curve
+        [~, ber(2,ii,jj)] = biterr(bits, rx);
+
+   end
+end
+
 
 %% plot
 
-ber = mean(ber,nIter); %take mean across all iterations
+ber = mean(ber,2); %take mean across all channels
 
 %plotting ber for different equalization techniques
-semilogy(EbNo,ber(1,:),'DisplayName','OFDM zf');
+semilogy(EbNo,ber(1,:),'DisplayName','zf')
+hold on;
+semilogy(EbNo,ber(2,:),'DisplayName','mmse');
 hold on;
 
 title('BER curves');
 xlabel('EbNo(dB)');
 ylabel('Bit Error Rate');
 legend('show');
-
-
-
-
-
-
